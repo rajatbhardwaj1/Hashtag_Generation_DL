a9f2ec2fbc2cd017f55bef43812c56cd0ce6ae0b
rajatbhardwaj1 <rb9878203799@gmail.com>
2022-05-27 14:54:39 +0530

    heruku deployment

---
+++ app.py
@@ -0,0 +1,21 @@
+from crypt import methods
+from flask import Flask, redirect, render_template , request, url_for
+from pyrfc3339 import generate
+
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET" , "POST"] )
+def my_form():
+    if request.method == "POST":
+        t = request.form["text"]
+        return redirect(url_for("my_form_post" , generated_text =  t  ))
+    else:
+        return render_template("index.html" )
+
+
+@app.route("/result/<generated_text>", methods = ["GET" ])
+def my_form_post(generated_text):
+    return render_template("result.html" , result = generated_text)
+
+
+app.run(debug= True)
